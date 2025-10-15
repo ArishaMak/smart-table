920cbb253631116153ba50ea00f72e7115267a0c
ArishaMak <makovchikarina@icloud.com>
2025-10-15 13:59:03 +0300

    Update searching.js

--- src/components/searching.js
+++ src/components/searching.js
@@ -1,11 +1,19 @@
-import {rules, createComparison} from "../lib/compare.js";
+/* import {rules, createComparison} from "../lib/compare.js"; */
 
-
+// ПОИСК
 export function initSearching(searchField) {
-    // @todo: #5.1 — настроить компаратор
+    /* // @todo: #5.1 — настроить компаратор
+    const compare = createComparison([
+        rules.skipEmptyTargetValues, // пропускаем пустые значения в state
+        rules.searchMultipleFields(searchField, ['date', 'customer', 'seller'], false) 
+        // searchField — имя поля в state
+        // массив — список полей, по которым ищем
+        // false — означает "без строгого совпадения" (можно частичное)
+    ]);*/
 
-    return (data, state, action) => {
-        // @todo: #5.2 — применить компаратор
-        return data;
-    }
+    return (query, state, action) => { // result заменили на query
+        return state[searchField] ? Object.assign({}, query, { // проверяем, что в поле поиска было что-то введено
+            search: state[searchField], // устанавливаем в query параметр
+        }) : query; // если поле с поиском пустое, просто возвращаем query без изменений
+    } 
 }
